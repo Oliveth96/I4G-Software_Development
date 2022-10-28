005865381cead77db913cd056fcaa1856b053a0f
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-10-28 16:03:26 +0100

    JavaScript

--- Lesson 6/index.js
+++ Lesson 6/index.js
@@ -1,8 +1,20 @@
+/**
+ *Declare a variable change and assigns a query that selects the html tag '<p></p>'
+ from the html document
+*/
 const change = document.querySelector('p');
 
+// Adds an event listener which runs the function changeName when clicked upon
 change.addEventListener('click', changeName);
 
+/**
+ * Function: that creates a constant which prompts users to enter a name. 
+ * The function stores the names typed in and changes the content of the <p></p> tag. 
+ * Recall we already assigned it to a constant - change, so the function changes the content of the constant
+ */
 function changeName() {
   const name = prompt('Enter a new name');
   change.textContent = `Player 1: ${name}`;
 }
+
+
